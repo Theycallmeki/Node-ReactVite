a4a230a3a0a15c5ba2e978b3f419de2f6509d7b1
Theycallmeki <laxamana.kian13@gmail.com>
2025-05-07 22:35:17 +0800

    1s

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Home = () => {
+  const handleGetStarted = () => {
+    // You can navigate or trigger an action here
+    alert('Get Started clicked!');
+  };
+
   return (
     <div>
       <h1>Welcome to the Homepage!</h1>
       <p>This is the home page of your app.</p>
+      <button onClick={handleGetStarted}>Get Started</button>
     </div>
   );
 };
